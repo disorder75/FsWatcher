a13d25b6a8e7fd302042cbd26dc03c2f0b8f6bd0
disorder75 <nnzcastelli@gmail.com>
2022-03-14 12:44:31 +0100

    - init project: 	- skeleton, empty job, just log application name every 15seconds

---
+++ src/test/java/eu/europa/acer/FsWatcher/FsWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.europa.acer.FsWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
